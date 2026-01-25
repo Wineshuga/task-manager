283dd5dbec89e92196dee56377b8d7617e21d44d
Wineshuga <uzochukwuwinnie@gmail.com>
2026-01-25 16:06:09 +0100

    Include all api routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,29 +1,24 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework.routers import DefaultRouter
+from projects.views import ProjectViewSet, TaskViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.views import RegisterView, MeView
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/login/', TokenObtainPairView.as_view()),
+    path("api/auth/register/", RegisterView.as_view()),
     path('api/auth/refresh/', TokenRefreshView.as_view()),
-    path('api/users', include('users.urls')),
+    path('api/auth/me/', MeView.as_view()),
+    path(
+        'api/projects/<int:project_id>/tasks/',
+        TaskViewSet.as_view({'get': 'list', 'post': 'create'}),
+        name='project-tasks'
+    ),
+    path('api/', include(router.urls)),
 ]
