a531e2c74f13ff278b217f22d9b472055003c51a
Wineshuga <uzochukwuwinnie@gmail.com>
2026-03-18 11:22:54 +0100

    Add metrics config to app.py

--- backend/projects/apps.py
+++ backend/projects/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
-
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
 
 class ProjectsConfig(AppConfig):
     name = 'projects'
+
+    def ready(self):
+        metric_reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
+        provider = MeterProvider(metric_readers=[metric_reader])
+
+        # Sets the global default meter provider
+        metrics.set_meter_provider(provider)
