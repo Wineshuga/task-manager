9354d1602d113c6ce56344871bcfac0baca4165e
Wineshuga <uzochukwuwinnie@gmail.com>
2026-04-10 00:44:27 +0100

    Modify config for collector

--- backend/projects/apps.py
+++ backend/projects/apps.py
@@ -1,20 +1,17 @@
 from django.apps import AppConfig
-import os
 
 class ProjectsConfig(AppConfig):
     name = 'projects'
 
     def ready(self):
-        if os.environ.get('RUN_MAIN') != 'true':
-            return
+        from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
         from opentelemetry import metrics
-        from opentelemetry.sdk.metrics import MeterProvider
-        from opentelemetry.exporter.prometheus import PrometheusMetricReader
+        from opentelemetry.sdk.metrics import MeterProvider 
+        from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
         from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-        from prometheus_client import start_http_server
 
-        resource = Resource.create(attributes={SERVICE_NAME: "task-manager"})
-        start_http_server(port=9464)
-        reader = PrometheusMetricReader()
-        provider = MeterProvider(resource=resource, metric_readers=[reader])
-        metrics.set_meter_provider(provider)
+        resource = Resource(attributes={SERVICE_NAME: "task-manager"})
+        exporter = OTLPMetricExporter(endpoint="http://localhost:4318/v1/metrics")
+        reader = PeriodicExportingMetricReader(exporter)
+        meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
+        metrics.set_meter_provider(meterProvider)
