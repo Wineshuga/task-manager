c791542504fb1d8761f076d499d60cef5579a760
Wineshuga <uzochukwuwinnie@gmail.com>
2026-05-12 23:25:32 +0100

    Remove hardcoded config

--- backend/projects/apps.py
+++ backend/projects/apps.py
@@ -4,14 +4,10 @@ class ProjectsConfig(AppConfig):
     name = 'projects'
 
     def ready(self):
-        from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
         from opentelemetry import metrics
         from opentelemetry.sdk.metrics import MeterProvider 
-        from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
         from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
         resource = Resource(attributes={SERVICE_NAME: "task-manager"})
-        exporter = OTLPMetricExporter(endpoint="http://localhost:4318/v1/metrics")
-        reader = PeriodicExportingMetricReader(exporter)
-        meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
+        meterProvider = MeterProvider(resource=resource)
         metrics.set_meter_provider(meterProvider)
