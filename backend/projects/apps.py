e1916c59efbf6866304670a827ab609f7f339866
Wineshuga <uzochukwuwinnie@gmail.com>
2026-05-14 22:19:06 +0100

    Modify collector to push metrics to prometheus

--- backend/projects/apps.py
+++ backend/projects/apps.py
@@ -1,13 +1,25 @@
 from django.apps import AppConfig
+from opentelemetry import metrics
 
 class ProjectsConfig(AppConfig):
     name = 'projects'
 
     def ready(self):
+        from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
         from opentelemetry import metrics
         from opentelemetry.sdk.metrics import MeterProvider 
+        from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
         from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+        resource = Resource(attributes={
+                SERVICE_NAME: "task-manager"
+            })
 
-        resource = Resource(attributes={SERVICE_NAME: "task-manager"})
-        meterProvider = MeterProvider(resource=resource)
-        metrics.set_meter_provider(meterProvider)
+        exporter = OTLPMetricExporter()
+        reader = PeriodicExportingMetricReader(exporter)
+
+        meter_provider = MeterProvider(
+                resource=resource,
+                metric_readers=[reader]
+            )
+
+        metrics.set_meter_provider(meter_provider)
